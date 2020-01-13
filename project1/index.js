5e1af7dec021f933c57f78d7ba3735e1d2844919
Mohammed Ali <alhiloempire@gmail.com>
2020-01-13 14:20:41 +0100

    ah

--- project1/index.js
+++ project1/index.js
@@ -1,62 +1,153 @@
 new Vue({
     el: '#app',
     data: {
-        tempnamn: " ",
+
         Namn: " ",
         bonus: 5,
         strpoints: 0,
-        Intpoints: 0,
-        Dexpoints: 0,
+        intpoints: 0,
+        dexpoints: 0,
         strstatus: 0,
         Intstatus: 0,
         Dexstatus: 0,
-
+        type: " ",
+        yes: false,
+        warr: false,
+        rog: false,
+        mage: false,
+        extra: false,
+        reset: false
     },
     methods: {
-        angenamn: function(){
-            this.tempnamn = event.target.value;
-        },
-        create: function () {
+
+        create() {
+            this.Namn = event.target.value;
+            this.type = " ";
             this.strstatus = 0;
             this.Intstatus = 0;
             this.Dexstatus = 0;
 
-            this.Namn = this.tempnamn;
             this.strstatus += Math.floor(Math.random() * 18) + 1;
             this.Intstatus += Math.floor(Math.random() * 18) + 1;
             this.Dexstatus += Math.floor(Math.random() * 18) + 1;
 
-            this.Dexstatus += this.Dexpoints;
-            this.Intstatus += this.Intpoints;
-            this.strstatus += this.strpoints;
+            this.type = "";
+            if (12 <= this.strstatus) {
+                this.type += "warrior" + " ";
+                this.warr = true;
+            }
+            if (12 <= this.Dexstatus) {
+                this.type += "rogue" + " ";
+                this.rog = true;
+            }
+            if (12 <= this.Intstatus) {
+                this.type += "mage" + " ";
+                this.mage = true;
+            } else if (this.Intstatus < 12 && this.Dexstatus < 12 && this.strstatus < 12) {
+                this.type += "anything with those stats";
+            }
+
+            this.yes = true;
+            this.extra = true;
+            this.reset = true;
+        },
+        resetpoints() {
+            this.bonus = 5;
+            this.strstatus = 0;
+            this.strpoints = 0;
+            this.Intstatus = 0;
+            this.intpoints = 0;
+            this.Dexstatus = 0;
+            this.dexpoints = 0;
+            this.warr = false;
+            this.mage = false;
+            this.rog = false;
+            this.type = "";
+            this.yes = false;
+            this.extra = false;
+            this.reset = false;
 
 
         },
-        strpointsclick: function () {
+        strpointsclick() {
 
             if (0 < this.bonus) {
                 this.bonus--;
+                this.strstatus++;
                 this.strpoints++;
+
+                this.type = "";
+                if (12 <= this.strstatus) {
+                    this.type += "warrior" + " ";
+                    this.warr = true;
+                }
+                if (12 <= this.Dexstatus) {
+                    this.type += "rogue" + " ";
+                    this.rog = true;
+                }
+                if (12 <= this.Intstatus) {
+                    this.type += "mage" + " ";
+                    this.mage = true;
+                } else if (this.Intstatus < 12 && this.Dexstatus < 12 && this.strstatus < 12) {
+                    this.type += "anything with those stats";
+                }
             } else if (this.bonus <= 0) {
-                this.bonus = "You have 0 points ";
+                this.bonus = "you have 0 points ";
+                this.reset = true;
             }
         },
-        intpointsclick: function () {
+        intpointsclick() {
 
             if (0 < this.bonus) {
                 this.bonus--;
-                this.Intpoints++;
+                this.Intstatus++;
+                this.intpoints++;
+
+                this.type = "";
+                if (12 <= this.strstatus) {
+                    this.type += "warrior" + " ";
+                    this.warr = true;
+                }
+                if (12 <= this.Dexstatus) {
+                    this.type += "rogue" + " ";
+                    this.rog = true;
+                }
+                if (12 <= this.Intstatus) {
+                    this.type += "mage" + " ";
+                    this.mage = true;
+                } else if (this.Intstatus < 12 && this.Dexstatus < 12 && this.strstatus < 12) {
+                    this.type += "anything with those stats";
+                }
             } else if (this.bonus <= 0) {
-                this.bonus = "You have 0 points ";
+                this.bonus = "you have 0 points ";
+                this.reset = true;
             }
-        }, 
-        dexpointsclick: function () {
+        },
+        dexpointsclick() {
 
             if (0 < this.bonus) {
                 this.bonus--;
-                this.Dexpoints++;
+                this.Dexstatus++;
+                this.dexpoints++;
+
+                this.type = "";
+                if (12 <= this.strstatus) {
+                    this.type += "warrior" + " ";
+                    this.warr = true;
+                }
+                if (12 <= this.Dexstatus) {
+                    this.type += "rogue" + " ";
+                    this.rog = true;
+                }
+                if (12 <= this.Intstatus) {
+                    this.type += "mage" + " ";
+                    this.mage = true;
+                } else if (this.Intstatus < 12 && this.Dexstatus < 12 && this.strstatus < 12) {
+                    this.type += "anything with those stats";
+                }
             } else if (this.bonus <= 0) {
-                this.bonus = "You have 0 points ";
+                this.bonus = "you have 0 points ";
+                this.reset = true;
             }
         }
     }
